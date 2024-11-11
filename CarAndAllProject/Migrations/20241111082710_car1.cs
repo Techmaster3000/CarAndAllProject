6387e2b70a98c5cb156c2f91bac779ba438925f0
Techmaster3000 <52240882+Techmaster3000@users.noreply.github.com>
2024-11-11 11:48:41 +0100

    Added Identity Login and 2FA

---
+++ CarAndAllProject/Migrations/20241111082710_car1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarAndAllProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class car1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vehicle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Merk = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Kenteken = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Kleur = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Aanschafjaar = table.Column<int>(type: "int", nullable: false),
+                    Soort = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicle", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vehicle");
+        }
+    }
+}
