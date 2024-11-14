7c6dabfc464ced5691d1ba6167416705eee85a59
Techmaster3000 <52240882+Techmaster3000@users.noreply.github.com>
2024-11-14 19:57:51 +0100

    Updated Bootstrap Added Dark Mode with footer button to switch.

--- CarAndAllProject/wwwroot/js/site.js
+++ CarAndAllProject/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function switchTheme() {
+    const themeBtnIcon = document.getElementById('themeIcn');
+
+    if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+        document.documentElement.setAttribute('data-bs-theme', 'light')
+        themeBtnIcon.classList.remove('fa-sun');
+        themeBtnIcon.classList.add('fa-moon');
+    }
+    else {
+        document.documentElement.setAttribute('data-bs-theme', 'dark')
+        themeBtnIcon.classList.remove('fa-moon');
+        themeBtnIcon.classList.add('fa-sun');
+        //change the class of the button
+
+    }
+}
+
+
